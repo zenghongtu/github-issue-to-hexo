5a10d1cfdc362b8ff6d1a588402073427ff5ab86
zenghongtu <zenghongtu@gmail.com>
2022-07-09 11:55:38 +0800

    convert all issues

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,56 @@ import * as github from '@actions/github';
 import Hexo from 'hexo';
 import { resolve } from 'path';
 
-const MILESTONE_PUBLISH = 'publish';
+const PAGE_SIZE = 100;
 
 async function run() {
 	try {
 		const owner = core.getInput('owner', { required: true });
 		const repo = core.getInput('repo', { required: true });
-		const issue_number = +core.getInput('issue_number', { required: true });
-
-		if (Number.isNaN(issue_number)) {
-			throw new Error('issue_number is incorrect!');
-		}
-
 		const token = core.getInput('token', { required: true });
-		const replace = core.getBooleanInput('replace', { required: false });
-		const output = core.getInput('output', { required: false }) || './';
 
-		const octokit = github.getOctokit(token);
-		const response = await octokit.rest.issues.get({ issue_number, owner, repo });
+		const milestone = core.getInput('milestone', { required: false });
+		const output = core.getInput('output', { required: false });
 
 		const hexo = new Hexo(resolve(process.cwd(), output));
 		await hexo.init();
-		const { title, updated_at: updated, labels, milestone, body: content, created_at: date } = response.data;
 
-		if (milestone?.title !== MILESTONE_PUBLISH) {
-			console.log(`Issue does not have milestone(${MILESTONE_PUBLISH})`);
-		} else {
-			const tags = labels.map((label: any) => label.name);
-			hexo.post.create(
-				{
-					title,
-					date,
-					updated,
-					tags,
-					content,
-					path: `${issue_number}`,
-				} as any,
-				replace
-			);
+		const octokit = github.getOctokit(token);
+
+		let hasMore = true;
+
+		while (hasMore) {
+			const { data: sourceData } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+				owner,
+				repo,
+				sort: 'created',
+				state: 'open',
+				creator: owner,
+				per_page: PAGE_SIZE,
+			});
+
+			console.log('issues length: ', sourceData.length);
+
+			hasMore = !(sourceData.length < PAGE_SIZE);
+
+			const data = sourceData.filter((item) => item.milestone?.title === milestone);
+
+			for (const item of data) {
+				const { title, updated_at: updated, labels, number, body: content, created_at: date } = item;
+
+				const tags = labels.map((label: any) => label.name);
+				await hexo.post.create(
+					{
+						title,
+						date,
+						updated,
+						tags,
+						content,
+						path: `${number}`,
+					} as any,
+					true
+				);
+			}
 		}
 	} catch (err: any) {
 		core.setFailed(err);
